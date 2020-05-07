d820a56dfc12812a23da52be038ea50a56ce35ab
Harry Merritt <harry@merritt.es>
2020-05-07 01:44:50 +0100

    Add fallback server error message

--- api/server.js
+++ api/server.js
@@ -10,4 +10,12 @@ server.use("/api/register", registerRouter);
 // server.use("/api/login", loginRouter);
 // server.use("/api/users", usersRouter);
 
+// Fallback server error message
+server.use((err, req, res, next) => {
+    console.log(err);
+    res.status(500).json({
+        message: "Somthing went wrong",
+    });
+});
+
 module.exports = server;
