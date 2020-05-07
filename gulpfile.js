61c8d6af9f5a9ac881ca548b8ea54b464643ce3c
Harry Merritt <harry@merritt.es>
2020-05-07 01:20:01 +0100

    Fix gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const prettier = require("gulp-prettier");
 
 gulp.task("default", () => {
     return gulp
-        .src("*.js")
+        .src(["index.js", "api/**/*.js", "data/**/*.js", "gulpfile.js"])
         .pipe(prettier({ editorconfig: true }))
         .pipe(gulp.dest((file) => file.base));
 });
